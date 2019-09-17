01bafe0fe110081163af8541ee7b40b79855f351
Antonio López Marín <tonibasket_p.c@hotmail.com>
2019-09-17 11:40:12 +0200

    Initial commit
    Release 0.1.0
    
    Remove repo directory
    
    Remove out folder
    
    Add ktlint
    
    Fill README
    
    Add test in the example
    
    Improve README example
    
    Prepare release
    
    First release version 0.1.1
    
    Come back 0.1.0
    
    Change to jitpack
    
    Fix travis
    
    Fix tests
    
    Add test when empty object
    
    Run task before compileKotlin and fix example test
    
    Release 0.1.1

--- plugin/src/test/kotlin/com/karumi/hagu/plugin/KotlinConfigGeneratorTest.kt
+++ plugin/src/test/kotlin/com/karumi/hagu/plugin/KotlinConfigGeneratorTest.kt
@@ -1,51 +1,53 @@
 package com.karumi.hagu.plugin
 
 import com.karumi.kotlinsnapshot.matchWithSnapshot
+import java.io.File
+import java.util.Properties
 import junit.framework.TestCase.assertTrue
 import org.gradle.internal.impldep.org.testng.annotations.BeforeTest
 import org.junit.Test
-import java.io.File
-import java.util.*
 
 class KotlinConfigGeneratorTest {
 
-    private val buildDir = File("build/test")
+  private val buildDir = File("build/test")
+  private val kotlinConfigGenerator = KotlinConfigGenerator(buildDir)
 
-    @BeforeTest
-    fun removeDirectories() {
-        buildDir.delete()
-    }
+  @BeforeTest
+  fun removeDirectories() {
+    buildDir.delete()
+  }
 
-    @Test
-    fun `generate kotlin configuration object with properties`() {
-        val kotlinConfigGenerator = KotlinConfigGenerator(buildDir)
+  @Test
+  fun `generate kotlin configuration object with properties`() {
+    val properties = Properties()
+    properties["property_number"] = "1"
+    properties["property_string"] = "\"Hello world\""
 
-        val properties = Properties()
-        properties["property_number"] = "1"
-        properties["property_string"] = "\"Hello world\""
+    kotlinConfigGenerator.generateConfig(properties)
 
-        kotlinConfigGenerator.generateConfig(properties)
+    readFile(kotlinConfigGenerator.haguConfigFile).matchWithSnapshot()
+  }
 
-        readFile(kotlinConfigGenerator.haguConfigFile).matchWithSnapshot()
-    }
+  @Test
+  fun `should create directory if not exist`() {
+    kotlinConfigGenerator.generateConfig(Properties())
 
-    @Test
-    fun `should create directory if not exist`() {
-        val kotlinConfigGenerator = KotlinConfigGenerator(buildDir)
+    assertTrue(kotlinConfigGenerator.generatedSourceOutput.exists())
+  }
 
-        kotlinConfigGenerator.generateConfig(Properties())
+  @Test
+  fun `should create a hagu config file`() {
+    kotlinConfigGenerator.generateConfig(Properties())
 
-        assertTrue(kotlinConfigGenerator.generatedSourceOutput.exists())
-    }
+    assertTrue(kotlinConfigGenerator.haguConfigFile.exists())
+  }
 
-    @Test
-    fun `should create a hagu config file`() {
-        val kotlinConfigGenerator = KotlinConfigGenerator(buildDir)
+  @Test
+  fun `should create an empty object if there aren't properties`() {
+    kotlinConfigGenerator.generateConfig(Properties())
 
-        kotlinConfigGenerator.generateConfig(Properties())
+    readFile(kotlinConfigGenerator.haguConfigFile).matchWithSnapshot()
+  }
 
-        assertTrue(kotlinConfigGenerator.haguConfigFile.exists())
-    }
-
-    private fun readFile(file: File): String = file.readText()
+  private fun readFile(file: File): String = file.readText()
 }
